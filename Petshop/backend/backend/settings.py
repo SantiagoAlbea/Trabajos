d420a908e416dc2cccd0133f2bf53bc4519ceb16
SantiagoAlbea <santiialbea123@gmail.com>
2021-03-31 15:25:57 -0300

    Hiding django key

--- Petshop/backend/backend/settings.py
+++ Petshop/backend/backend/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rtl%a89mf@4pu6zvy0_d#^*frx_sb-z2!p6n@v(!xei7bm&%md'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
